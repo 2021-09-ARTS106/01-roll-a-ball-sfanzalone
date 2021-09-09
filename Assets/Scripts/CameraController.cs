6c6275449949b855493fb3f9891d05463efa9933
sfanzalone <sfanzalone@gmail.com>
2021-09-09 10:06:26 -0400

    Just resolved Camera Controller Error

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,18 +5,18 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public GameObject player;
-    
+
     private Vector3 offset;
 
     // Start is called before the first frame update
     void Start()
     {
-        offset = transform.position - player.trasform.position;
+        offset = transform.position - player.transform.position;
     }
 
     // Update is called once per frame
-    void lateUpdate()
+    void LateUpdate()
     {
-        transform.position = player.trasform.position + offset;
+        transform.position = player.transform.position + offset;
     }
 }
